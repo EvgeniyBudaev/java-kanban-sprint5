fe41ba8d2a9e0e3b8004a53b19596d075ccf3ebf
Evgeniy Budaev <budaev.e@gmail.com>
2022-08-27 16:33:03 +0300

    Comments

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -8,7 +8,7 @@ public class InMemoryHistoryManager implements HistoryManager {
     private static final int LIMIT_HISTORY_TASKS = 10;
     private final Map<Integer, Task> historyTasks = new LinkedHashMap<>();
 
-    //Добавление нового просмотра задачи в историю
+    // Добавление нового просмотра задачи в историю
     @Override
     public void add(Task task) {
         if (task != null) {
@@ -23,12 +23,13 @@ public class InMemoryHistoryManager implements HistoryManager {
         }
     }
 
-    //Удаление просмотра из истории
+    // Удаление просмотра из истории
     @Override
     public void remove(int id) {
         historyTasks.remove(id);
     }
 
+    // Получение истории просмотров
     @Override
     public List<Task> getHistory() {
         return new LinkedList<>(historyTasks.values());
